8d77c8bb790e942935169caa6dc336af26da92eb
Mchrispin <mchrispin14@gmail.com>
2025-06-26 14:27:32 +0200

    added calculator with ganache local blockchain

--- Calculator/contracts/Calculator.sol
+++ Calculator/contracts/Calculator.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.21;
+pragma solidity >=0.4.22 <0.9.0;
 
 
 contract Calculator {
     string public message;
 
-    constructor() {
+    constructor() public {
         message = "Hello blockchain";
     }
 
--- Calculator/src/utils/web3.ts
+++ Calculator/src/utils/web3.ts
@@ -1,67 +1,8 @@
 import Web3 from "web3";
+import ContractJSON from "../abis/Calculator.json";
 
-const contractAddress = "0x293C4811A558717f2065E4BF3b1A58894F842051";
-const contractABI = [
-  {
-    inputs: [],
-    stateMutability: "nonpayable",
-    type: "constructor",
-  },
-  {
-    inputs: [],
-    name: "message",
-    outputs: [{ internalType: "string", name: "", type: "string" }],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [{ internalType: "string", name: "_message", type: "string" }],
-    name: "setMessage",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [
-      { internalType: "uint256", name: "a", type: "uint256" },
-      { internalType: "uint256", name: "b", type: "uint256" },
-    ],
-    name: "add",
-    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
-    stateMutability: "pure",
-    type: "function",
-  },
-  {
-    inputs: [
-      { internalType: "uint256", name: "a", type: "uint256" },
-      { internalType: "uint256", name: "b", type: "uint256" },
-    ],
-    name: "subtract",
-    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
-    stateMutability: "pure",
-    type: "function",
-  },
-  {
-    inputs: [
-      { internalType: "uint256", name: "a", type: "uint256" },
-      { internalType: "uint256", name: "b", type: "uint256" },
-    ],
-    name: "multiply",
-    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
-    stateMutability: "pure",
-    type: "function",
-  },
-  {
-    inputs: [
-      { internalType: "uint256", name: "a", type: "uint256" },
-      { internalType: "uint256", name: "b", type: "uint256" },
-    ],
-    name: "divide",
-    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
-    stateMutability: "pure",
-    type: "function",
-  },
-];
+let contractAddress;
+let contractABI;
 
 let web3: any;
 let contract: any;
@@ -70,6 +11,13 @@ export const initWeb3 = async () => {
   if ((window as any).ethereum) {
     web3 = new Web3((window as any).ethereum);
     await (window as any).ethereum.request({ method: "eth_requestAccounts" });
+    contractABI = ContractJSON.abi;
+    const networkId = await web3.eth.net.getId();
+    const deployedNetwork = (ContractJSON.networks as any)[networkId];
+    if(!deployedNetwork) throw new Error(`Contract not deployed on network with ID ${networkId}`);
+    contractAddress = deployedNetwork.address;
+
+
     contract = new web3.eth.Contract(contractABI, contractAddress);
   } else {
     alert("Please install MetaMask!");
@@ -77,26 +25,44 @@ export const initWeb3 = async () => {
 };
 
 export const getMessage = async () => {
-  return await contract.methods.message().call();
+  return await contract.methods.message().call({
+    gasPrice: await web3.eth.getGasPrice(),
+  })
 };
 
 export const add = async (a: number, b: number) => {
-  return await contract.methods.add(a, b).call();
+  return await contract.methods.add(a, b).call({
+    gasPrice: await web3.eth.getGasPrice()
+  })
 };
 
 export const subtract = async (a: number, b: number) => {
-  return await contract.methods.subtract(a, b).call();
+  return await contract.methods.subtract(a, b).call({
+    gasPrice: await web3.eth.getGasPrice()
+  })
 };
 
 export const multiply = async (a: number, b: number) => {
-  return await contract.methods.multiply(a, b).call();
+  return await contract.methods.multiply(a, b).call({
+    gasPrice: await web3.eth.getGasPrice()
+  })
 };
 
 export const divide = async (a: number, b: number) => {
-  return await contract.methods.divide(a, b).call();
+  return await contract.methods.divide(a, b).call({
+    gasPrice: await web3.eth.getGasPrice()
+  })
 };
 
 export const setMessage = async (newMessage: any) => {
   const accounts = await web3.eth.getAccounts();
-  await contract.methods.setMessage(newMessage).send({ from: accounts[0] });
-};
+  if(accounts.length === 0) throw new Error("No accounts found. Please connect your wallet.");
+  if(!newMessage || typeof newMessage !== 'string') throw new Error("Invalid message. Please provide a valid string.");
+  if(newMessage.length > 20) throw new Error("Message too long. Please limit to 20 characters.");
+
+  await contract.methods.setMessage(newMessage).send({
+    from: accounts[0],
+    gas: 3000000,
+    gasPrice: await web3.eth.getGasPrice()
+  })
+ };
