bb805ebb4fc866b109dcdcaed44e82962bacf862
Mchrispin <mchrispin14@gmail.com>
2025-06-26 16:07:39 +0200

    added sepolia deployment

--- Calculator/truffle-config.js
+++ Calculator/truffle-config.js
@@ -1,3 +1,6 @@
+require('dotenv').config(); // Load environment variables from .env file
+const HDWalletProvider = require('@truffle/hdwallet-provider'); // Import HDWalletProvider for managing accounts
+
 module.exports = {
   networks: {
     development: {
@@ -14,6 +17,17 @@ module.exports = {
       host: "127.0.0.1",     // Localhost
       port: 7545,            // Ganache or local blockchain port
       network_id: "*",       // Match any network id
+    },
+    sepolia: {
+      provider: () => new HDWalletProvider(
+        [process.env.PRIVATE_KEY], // Use your private key from .env
+        process.env.SEPOLIA_RPC_URL // Use your Sepolia RPC URL from .env
+      ),
+      network_id: 11155111, // Sepolia's network ID
+      gas: 4465030,         // Gas limit for transactions
+      gasPrice: 10000000000, // 10 Gwei (adjust as needed
+      confirmations: 2,     // Number of confirmations to wait between deployments
+      timeoutBlocks: 200,   // Number of blocks before a deployment times out
     }
   },
 
